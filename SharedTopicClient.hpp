dec35fdda322f1a8d6389e98e02517d53e357710
Jiu-xiao <2592509183@qq.com>
2025-10-10 22:24:48 +0800

    Support domain-scoped topics via TopicConfig; replace topic_name with topic_configs.

--- SharedTopicClient.hpp
+++ SharedTopicClient.hpp
@@ -7,9 +7,9 @@ constructor_args:
   - uart_name: "uart_cdc"
   - task_stack_depth: 2048
   - buffer_size: 256
-  - topic_names:
+  - topic_configs:
     - "topic1"
-    - "topic2"
+    - ["topic2", "libxr_def_domain"]
 template_args: []
 required_hardware: uart_name
 depends: []
@@ -19,31 +19,47 @@ depends: []
 #include "app_framework.hpp"
 #include "uart.hpp"
 
-class SharedTopicClient : public LibXR::Application {
+class SharedTopicClient : public LibXR::Application
+{
  public:
-  typedef struct {
+  typedef struct
+  {
     SharedTopicClient *client;
     uint32_t topic_crc32;
     uint32_t index;
   } CallbackInfo;
 
-  SharedTopicClient(LibXR::HardwareContainer &hw,
-                    LibXR::ApplicationManager &app, const char *uart_name,
-                    uint32_t task_stack_depth, uint32_t buffer_size,
-                    std::initializer_list<const char *> topic_names)
+  struct TopicConfig
+  {
+    const char *name;
+    const char *domain = "libxr_def_domain";
+
+    TopicConfig(const char *name) : name(name) {}
+
+    TopicConfig(const char *name, const char *domain) : name(name), domain(domain) {}
+  };
+
+  SharedTopicClient(LibXR::HardwareContainer &hw, LibXR::ApplicationManager &app,
+                    const char *uart_name, uint32_t task_stack_depth,
+                    uint32_t buffer_size,
+                    std::initializer_list<TopicConfig> topic_configs)
       : uart_(hw.template Find<LibXR::UART>(uart_name)),
         tx_buffer_(new uint8_t[buffer_size], buffer_size),
-        tx_queue_(buffer_size) {
+        tx_queue_(buffer_size)
+  {
     ASSERT(uart_ != nullptr);
 
-    topics_pack_buffer_ = new LibXR::RawData[topic_names.size()];
+    topics_pack_buffer_ = new LibXR::RawData[topic_configs.size()];
 
     uint32_t i = 0;
 
-    for (auto name : topic_names) {
-      auto ans = LibXR::Topic::Find(name);
-      if (ans == nullptr) {
-        XR_LOG_ERROR("Topic not found: %s", name);
+    for (auto config : topic_configs)
+    {
+      auto domain = LibXR::Topic::Domain(config.domain);
+      auto ans = LibXR::Topic::Find(config.name, &domain);
+      if (ans == nullptr)
+      {
+        XR_LOG_ERROR("Topic not found: %s/%s", config.domain, config.name);
         ASSERT(false);
       }
       topics_pack_buffer_[i] = LibXR::RawData(
@@ -51,20 +67,19 @@ class SharedTopicClient : public LibXR::Application {
           ans->data_.max_length + LibXR::Topic::PACK_BASE_SIZE);
 
       void (*func)(bool, CallbackInfo, LibXR::RawData &) =
-          [](bool in_isr, CallbackInfo info, LibXR::RawData &data) {
-            LibXR::WriteOperation op;
-            LibXR::Topic::PackData(info.topic_crc32,
-                                   info.client->topics_pack_buffer_[info.index],
-                                   data);
-            info.client->tx_queue_.PushBatch(
-                static_cast<uint8_t *>(
-                    info.client->topics_pack_buffer_[info.index].addr_),
-                info.client->topics_pack_buffer_[info.index].size_);
-            info.client->tx_sem_.PostFromCallback(in_isr);
-          };
+          [](bool in_isr, CallbackInfo info, LibXR::RawData &data)
+      {
+        LibXR::WriteOperation op;
+        LibXR::Topic::PackData(info.topic_crc32,
+                               info.client->topics_pack_buffer_[info.index], data);
+        info.client->tx_queue_.PushBatch(
+            static_cast<uint8_t *>(info.client->topics_pack_buffer_[info.index].addr_),
+            info.client->topics_pack_buffer_[info.index].size_);
+        info.client->tx_sem_.PostFromCallback(in_isr);
+      };
 
-      auto msg_cb = LibXR::Topic::Callback::Create(
-          func, CallbackInfo{this, ans->data_.crc32, i});
+      auto msg_cb =
+          LibXR::Topic::Callback::Create(func, CallbackInfo{this, ans->data_.crc32, i});
 
       LibXR::Topic topic(ans);
 
@@ -73,25 +88,27 @@ class SharedTopicClient : public LibXR::Application {
       i++;
     }
 
-    tx_thread_.Create(this, TxThreadFun, "SharedTopicClientTxThread",
-                      task_stack_depth, LibXR::Thread::Priority::REALTIME);
+    tx_thread_.Create(this, TxThreadFun, "SharedTopicClientTxThread", task_stack_depth,
+                      LibXR::Thread::Priority::REALTIME);
 
     app.Register(*this);
   }
 
-  static void TxThreadFun(SharedTopicClient *client) {
+  static void TxThreadFun(SharedTopicClient *client)
+  {
     LibXR::Semaphore write_op_sem;
     LibXR::WriteOperation op(write_op_sem);
     LibXR::WriteOperation op_none;
-    while (true) {
+    while (true)
+    {
       client->tx_sem_.Wait();
-      auto size =
-          LibXR::min(client->tx_queue_.Size(), client->tx_buffer_.size_);
-      if (size > 0 && client->tx_queue_.PopBatch(
-                          static_cast<uint8_t *>(client->tx_buffer_.addr_),
-                          size) == ErrorCode::OK) {
-        client->uart_->Write(
-            {static_cast<uint8_t *>(client->tx_buffer_.addr_), size}, op_none);
+      auto size = LibXR::min(client->tx_queue_.Size(), client->tx_buffer_.size_);
+      if (size > 0 &&
+          client->tx_queue_.PopBatch(static_cast<uint8_t *>(client->tx_buffer_.addr_),
+                                     size) == ErrorCode::OK)
+      {
+        client->uart_->Write({static_cast<uint8_t *>(client->tx_buffer_.addr_), size},
+                             op_none);
       }
     }
   }
